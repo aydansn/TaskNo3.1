19e6dfa2f0c6480e875efc0f15c2efb303ad17f5
aydansn <97697518+aydansn@users.noreply.github.com>
2024-05-23 08:13:44 +0400

    Add files via upload

---
+++ Task3/script.js
@@ -0,0 +1,64 @@
+// ----------------------Task1------------------------------------
+
+const country = "Azerbaijan";
+console.log(country);
+
+let region = "Baku";
+console.log(region);
+
+let populasiya = 10000;
+console.log(populasiya);
+
+// ----------------------Task2------------------------------------
+//------------------------a)------------------
+const olkeAdidir = true;
+if(olkeAdidir===true){
+    console.log("Duzdur");
+}
+else{
+    console.log("Yanlishdir");
+}
+
+//------------------------b)------------------
+let dil = "English";
+console.log(typeof dil);
+ dil = 23;
+console.log(typeof dil);
+dil = true;
+console.log(typeof dil);
+dil = false;
+console.log(typeof dil);
+
+// ----------------------Task3----------------------------------
+
+let language = "Azerbaijan";
+console.log(language);
+
+
+// ----------------------Task4----------------------------------
+let numberSay = 10000;
+console.log(numberSay/2);
+ numberSay = (numberSay/2 + 1);
+ console.log(numberSay);
+
+ let FinlandiyaEhali = 600000;
+ let AzerbaijanEhali = 10000;
+ console.log(FinlandiyaEhali > AzerbaijanEhali);
+ console.log(FinlandiyaEhali < AzerbaijanEhali);
+
+
+
+// ---------------------Challange---------------------------------
+const mehemmedCeki = 95;
+const mehemmedBoy = 1.88;
+const elcinCeki = 85;
+const elcinBoy = 1.76;
+
+const mehemmedBmi = (mehemmedCeki/mehemmedBoy*mehemmedBoy);
+console.log(mehemmedBmi);
+const elcinBmi = (elcinCeki/elcinBoy*elcinBoy);
+console.log(elcinBmi);
+
+console.log(mehemmedBmi>elcinBmi);
+console.log(mehemmedBmi<elcinBmi);
+
